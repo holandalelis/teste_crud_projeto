753c558c345c7b70e2f8479d0ef31b5a54084996
pedro <pedrolelis@hotmail.com>
2022-02-05 11:02:24 -0300

    altera e corrige bug que retirei funções da classe ContatoDAO

--- src/br/com/agenda/dao/ContatoDAO.java
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -49,7 +49,23 @@ public class ContatoDAO {
             conn = ConnectionFactory.createConnectionToMySQL();
             pstm = (PreparedStatement) conn.prepareStatement(sql);
             pstm.setString(1, contato.getNome());
-            pstm.setInt(2, contato.getId());
+            pstm.setInt(2, contato.getIdade());
+            pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+            pstm.setInt(4, contato.getId());
+            pstm.execute();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(pstm!=null){
+                    pstm.close();
+                }
+                if (conn!=null){
+                    conn.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
