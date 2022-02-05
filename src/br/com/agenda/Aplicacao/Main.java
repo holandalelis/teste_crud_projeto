028a1ce0cbde4642e166f081ff4ad249542fb29d
pedro <pedrolelis@hotmail.com>
2022-02-05 11:30:53 -0300

    finaliza projeto crud

--- src/br/com/agenda/Aplicacao/Main.java
+++ src/br/com/agenda/Aplicacao/Main.java
@@ -13,14 +13,17 @@ public class Main {
         contato.setNome("Pedro Holanda");
         contato.setIdade(28);
         contato.setDataCadastro(new Date());
-        //contatoDAO.save(contato); comentado apenas para não criar em duplicidade após o update
+        contatoDAO.save(contato);
+
         Contato c1 = new Contato();
         c1.setNome("Pedro Holanda Lelis");
         c1.setIdade(29);
         c1.setDataCadastro(new Date());
-        c1.setId(1);//numero no banco mysql da Primary Key
-        //contatoDAO.update(c1); comentando apenas para testar o método updade
-        contatoDAO.deleteByID(1);//numero do id utilizado no banco mysql
+        c1.setId(1);//numero do id utilizado no banco mysql para testar se estava funcionando
+        contatoDAO.update(c1);
+
+        contatoDAO.deleteByID(1);//numero do id utilizado no banco mysql para testar se estava funcionando
+
         for(Contato c : contatoDAO.getContatos()){
             System.out.println("Contato: " + c.getNome());
         }
